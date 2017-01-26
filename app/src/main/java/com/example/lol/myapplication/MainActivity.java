39418d8ceb0afe0c1a5f3f07eb0b909543025d0e
Pierre LESPINASSE <pilespin@e3r1p9.42.fr>
2017-01-26 15:26:22 +0100

    Build other activity and Intent

--- app/src/main/java/com/example/lol/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lol/myapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.lol.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.lol.myapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
