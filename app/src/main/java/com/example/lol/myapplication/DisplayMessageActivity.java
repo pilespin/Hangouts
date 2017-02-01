2e62aaeeaf4bb82ee4e3e7fef19d6e8ba5ac7972
Pierre LESPINASSE <pilespin@e3r10p5.42.fr>
2017-02-01 06:47:08 +0100

    Test database

--- app/src/main/java/com/example/lol/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/lol/myapplication/DisplayMessageActivity.java
@@ -1,8 +1,10 @@
 package com.example.lol.myapplication;
 
+import android.content.ContentValues;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -26,6 +28,22 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+
+
+
+        // Gets the data repository in write mode
+//        dbHelper db = new dbHelper();
+        dbHelper dbh = new dbHelper(this);
+
+        SQLiteDatabase db = dbh.getWritableDatabase();
+//
+// Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(dbHelper.FeedEntry.COLUMN_NAME_TITLE, "title");
+        values.put(dbHelper.FeedEntry.COLUMN_NAME_SUBTITLE, "subtitle");
+
+// Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(dbHelper.FeedEntry.TABLE_NAME, null, values);
 
     }
 }
--- app/src/main/java/com/example/lol/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lol/myapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.lol.myapplication;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -18,6 +23,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        dbHelper dbh = new dbHelper(this);
+
+        SQLiteDatabase db = dbh.getWritableDatabase();
+
+//    SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+// you will actually use after this query.
+        String[] projection = {
+                dbHelper.FeedEntry._ID,
+                dbHelper.FeedEntry.COLUMN_NAME_TITLE,
+                dbHelper.FeedEntry.COLUMN_NAME_SUBTITLE
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = dbHelper.FeedEntry.COLUMN_NAME_TITLE + " = ?";
+        String[] selectionArgs = { "Title" };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder = dbHelper.FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
+
+        Cursor cursor = db.query(
+                dbHelper.FeedEntry.TABLE_NAME,                     // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        List itemIds = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            long itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(dbHelper.FeedEntry._ID));
+            itemIds.add(itemId);
+            Log.d("------ MY LOG ------ : ", "test");
+
+        }
+        cursor.close();
+
+
+
+
     }
 
     public void createContact(View view) {
@@ -25,4 +79,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, createContact.class);
         startActivity(intent);
     }
+
 }
