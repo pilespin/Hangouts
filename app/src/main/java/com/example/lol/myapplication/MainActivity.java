d4c6dfe7e07fa6957606bb02e95aaca2e89c6aed
Pierre LESPINASSE <pilespin@e3r2p22.42.fr>
2017-02-03 19:19:15 +0100

    test view list

--- app/src/main/java/com/example/lol/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lol/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -68,6 +70,15 @@ public class MainActivity extends AppCompatActivity {
         return (allContact);
     }
 
+    ListView mListView;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,6 +100,16 @@ public class MainActivity extends AppCompatActivity {
 
         }
         Log.d("------ MY LG ------ : ", "AFTER WHILE PASSED");
+        ///////////////////////////////////////////////////////
+        mListView = (ListView) findViewById(R.id.listView);
+
+        //android.R.layout.simple_list_item_1 est une vue disponible de base dans le SDK android,
+        //Contenant une TextView avec comme identifiant "@android:id/text1"
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
+
     }
 
     public void createContact(View view) {
